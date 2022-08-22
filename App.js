8268edba7fffcbd4cc334ea9433dfd915a2b9acf
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-08-22 19:17:45 +0300

    added tailwindscss and began using it on the powerUp.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import { TailwindProvider } from "tailwindcss-react-native";
 import { StatusBar } from "expo-status-bar";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import StartMenu from "./Components/StartMenu";
+import StartMenu from "./components/StartMenu";
 
 function App() {
   return (
-    <View style={styles.container}>
-      <StartMenu />
-    </View>
+    <TailwindProvider>
+      <View style={styles.container}>
+        <StartMenu />
+      </View>
+    </TailwindProvider>
   );
 }
 
--- Components/PowerUp.js
+++ Components/PowerUp.js
@@ -1,25 +1,27 @@
-import { useState } from "react";
+import { TailwindProvider } from "tailwindcss-react-native";
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 function PowerUp(props) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{props.player}</Text>
+    <TailwindProvider>
+      <View className="bg-blue-400">
+        <Text style={styles.title}>{props.player}</Text>
 
-      <Text>{props.powerUp}</Text>
-      <Pressable style={styles.imageContainer}>
-        <Image
-          source={require("../assets/powerUps/flipAll.png")}
-          style={styles.powerUpImage}
-        ></Image>
-      </Pressable>
-    </View>
+        <Text>{props.powerUp}</Text>
+        <Pressable className="h-20 w-10 bg-slate-400">
+          <Image
+            source={require("../assets/powerUps/flipAll.png")}
+            className="h-20 w-10 object-scale-down"
+          ></Image>
+        </Pressable>
+      </View>
+    </TailwindProvider>
   );
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "blue",
+    height: 100,
     width: 200,
-    alignItems: "center",
   },
   title: {
     fontSize: 30,
